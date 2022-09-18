387656bbd8d979ed9c342df95b2253ed61264d91
sudhagarmsd5 <sudhagarmsd@gmail.com>
2022-09-18 15:27:18 +0530

    added tailwindcss prettier eslint and generated modules and components.

--- todoApp/Front-end/src/app/app-routing.module.ts
+++ todoApp/Front-end/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './shared/login-page/login-page.component';
+import { SignupPageComponent } from './shared/signup-page/signup-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login-page', pathMatch: 'full' },
+  { path: 'login-page', component: LoginPageComponent },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./modules/home/home.module').then(m => m.HomeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- todoApp/Front-end/src/app/app.module.ts
+++ todoApp/Front-end/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './shared/login-page/login-page.component';
+import { SignupPageComponent } from './shared/signup-page/signup-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LoginPageComponent, SignupPageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
